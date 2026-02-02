4b2c28da584c0580eb7cc87ab936fa5cb102feae
Yvan Lourenco <yvan.lourenco@gmail.com>
2026-02-02 15:43:07 +0100

    Set no neck config

--- nvim/.config/nvim/lua/mk0v/lazy/no-neck-pain.lua
+++ nvim/.config/nvim/lua/mk0v/lazy/no-neck-pain.lua
@@ -1,4 +1,36 @@
 return {
   'shortcuts/no-neck-pain.nvim',
-  version = '*'
+  version = '*',
+  config = function()
+    local noneck = require('no-neck-pain')
+
+    noneck.setup({
+      buffers = {
+
+        right = {
+          enabled = true,
+          scratchPad = {
+            enabled = true,
+            -- location = "~/notes/",
+            -- fileName = "dev-notes.md"
+            pathToFile = "~/notes/dev-notes.md" -- Use direct path to file
+          },
+        },
+
+        scratchPad = {
+          enabled = true,
+          location = "~/notes/scratch-pad/"
+        },
+
+        bo = {
+          filetype = "md"
+        }
+      },
+      width = 100
+    })
+
+    vim.keymap.set('n', '<leader>nn', noneck.toggle)
+    vim.keymap.set('n', '<leader>nh', function() noneck.toggle_side('left') end)
+    vim.keymap.set('n', '<leader>n;', function() noneck.toggle_side('right') end)
+  end
 }
--- nvim/.config/nvim/lua/mk0v/lazy_init.lua
+++ nvim/.config/nvim/lua/mk0v/lazy_init.lua
@@ -209,3 +209,11 @@ cmp.setup {
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+--
+-- Add rounded border to LSP hover
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+  vim.lsp.handlers.hover,
+  {
+    border = "rounded",
+  }
+)
